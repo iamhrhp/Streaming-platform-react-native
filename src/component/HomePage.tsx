83a178bf8736a34aa7be8551aaaaf6689b014641
iamhrhp <hrhp007@gmail.com>
2023-08-06 06:18:39 +0530

    [PK] fixed header of the pages

--- src/component/HomePage.tsx
+++ src/component/HomePage.tsx
@@ -1,12 +1,20 @@
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import MovieCardByGenre from './MovieCardByGenre/MovieCardByGenre';
 
-const HomePage = () => {
+const HomePage = ({setIsDarkTheme}: any) => {
+  console.log('---', setIsDarkTheme);
+
   return (
-    <View>
+    <View style={styles.mainView}>
       <MovieCardByGenre Genre="Action" tv={false} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainView: {
+    backgroundColor: '#100f13',
+  },
+});
 
 export default HomePage;
