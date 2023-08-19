a009ad91e93261731ea1095d4e9fdfc0f3980607
iamhrhp <hrhp007@gmail.com>
2023-08-20 04:16:05 +0530

    [PK] added flatlistsearchpage

---
+++ src/component/common components/DetailsPage.tsx
@@ -0,0 +1,11 @@
+import {View} from 'react-native';
+
+const DetailsPage = () => {
+  return (
+    <View>
+      <View></View>
+    </View>
+  );
+};
+
+export default DetailsPage;
---
+++ src/component/common components/FlatListSearchPage/FlatListSearchPage.tsx
@@ -0,0 +1,51 @@
+import {Text, View, Image, StyleSheet} from 'react-native';
+import {FC} from 'react';
+import MIcon from 'react-native-vector-icons/MaterialIcons';
+
+// interface IProps {}
+
+const FlatListSearchPage = (data: any) => (
+  <View style={styles.flatListContainer}>
+    <Image
+      style={styles.flatListImage}
+      alt="img"
+      source={{
+        uri: `http://image.tmdb.org/t/p/w500/${data.data.item.backdrop_path}`,
+      }}
+    />
+    <Text style={styles.flatListTitle}>{data.data.item.title}</Text>
+    <MIcon
+      name="keyboard-arrow-right"
+      color="white"
+      style={styles.flatListarrowIcon}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  flatListTitle: {
+    textAlign: 'left',
+    color: 'white',
+    marginLeft: 10,
+    flex: 8,
+  },
+  flatListarrowIcon: {
+    flex: 1,
+    marginLeft: 5,
+  },
+  flatListContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  flatListImage: {
+    flex: 4,
+    width: 120,
+    height: 60,
+    margin: 2,
+    borderRadius: 8,
+  },
+});
+
+export default FlatListSearchPage;
